c73997d1d91ef6d537f12927db87b509ad617aca
ApostolOleg <apostololeg0@gmail.com>
2025-12-16 12:29:48 +0200

    minor changes

--- src/components/Home/Description.jsx
+++ src/components/Home/Description.jsx
@@ -6,11 +6,11 @@ export default function Description({ quiz, onClose }) {
 			<div className="fixed inset-0 bg-black opacity-50 z-20" onClick={onClose} />
 			<div
 				className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2
-					flex flex-col min-h-[30vh] max-h-[80vh] w-[80vw] bg-[rgb(233,14,178)] rounded-2xl p-6 z-30 "
+					flex flex-col min-h-[60vh] max-h-[80vh] w-[80vw] bg-[rgb(233,14,178)] rounded-2xl p-6 z-30"
 			>
 				<div className="flex flex-col flex-1 overflow-y-auto gap-2">
-					<div className="text-2xl font-bold">{quiz.title}</div>
-					<div>{quiz.description}</div>
+					<div className="text-5xl font-bold">{quiz.title}</div>
+					<div className="text-3xl w-full break-all">{quiz.description}</div>
 				</div>
 
 				<div className="flex justify-between">
