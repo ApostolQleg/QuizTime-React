c6a03b9edc9435c9c3c4209507a36d77fefdf8e3
ApostolOleg <apostololeg0@gmail.com>
2025-12-25 19:15:49 +0200

    added smooth scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
@@ -7,6 +8,14 @@ import Edit from "./pages/Edit.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 export default function App() {
+	// Scroll to top on route change
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+	}, [pathname]);
+
+	// Main app layout with routing
 	return (
 		<div className="min-h-screen bg-[rgb(80,12,63)] text-[rgb(27,0,25)] flex flex-col flex-1">
 			<Header />
