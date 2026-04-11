10f875f9a0f53eeeaa6aafdc3aaccaf905730c23
ApostolOleg <apostololeg0@gmail.com>
2026-04-11 13:13:30 +0300

    refactor: update enqueue, dequeue, and peek methods to use 'end/start' terminology (#56)

--- src/shared/libs/deque.js
+++ src/shared/libs/deque.js
@@ -15,36 +15,27 @@ export class Deque {
 		return this.items;
 	}
 
-	enqueue(item, type = "last") {
-		if (type === "last") {
-			this.items.push(item);
-		}
-
-		if (type === "first") {
-			this.items.unshift(item);
-		}
+	enqueue(item, type = "end") {
+		if (type === "end") this.items.push(item);
+		else if (type === "start") this.items.unshift(item);
+		else throw new TypeError(`Invalid type: "${type}". Expected: end/start`);
 	}
 
 	_findIndex(type) {
-		if (type === "oldest") return 0;
-		if (type === "newest") return this.size - 1;
-
-		throw new TypeError(`Invalid type: "${type}". Expected: newest/oldest`);
+		if (type === "start") return 0;
+		else if (type === "end") return this.size - 1;
+		else throw new TypeError(`Invalid type: "${type}". Expected: end/start`);
 	}
 
-	dequeue(type = "oldest") {
+	dequeue(type = "end") {
 		if (this.isEmpty()) return null;
-
 		const index = this._findIndex(type);
-
 		return this.items.splice(index, 1)[0];
 	}
 
-	peek(type = "oldest") {
+	peek(type = "end") {
 		if (this.isEmpty()) return null;
-
 		const index = this._findIndex(type);
-
 		return this.items[index];
 	}
 }
