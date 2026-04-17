a334b2c7620d49a1353e02fadb8378fd88ddbc9a
ApostolOleg <apostololeg0@gmail.com>
2026-04-17 14:58:15 +0300

    feat: update footer logo styling for improved layout and accessibility

--- src/widgets/footer/ui/Footer.jsx
+++ src/widgets/footer/ui/Footer.jsx
@@ -3,8 +3,8 @@ import teamlogo from "@/shared/assets/piska_team_blue.png";
 export default function Footer() {
 	return (
 		<footer className="flex flex-row justify-center items-center mt-auto text-center p-6 text-sm shadow-[0_-10px_40px_rgba(0,0,0,0.3)] bg-(--col-bg-card) text-(--col-text-muted) border-t border-(--col-border)">
-			<div className="w-3xs">
-				<img src={teamlogo} />
+			<div className="w-40">
+				<img src={teamlogo} alt="Piska team logo" className="w-full h-auto" />
 			</div>
 			<div className="flex flex-col">
 				&copy; Piska team, 2025
